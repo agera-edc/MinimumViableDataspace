eb62e1c694b774c849996a9ef260ad49018c5c4f
Alexandre Gattiker <algattik@microsoft.com>
2022-07-21 15:28:08 +0200

    adapted CLI call in VerifyLocalTests

--- system-tests/resources/register-participants.sh
+++ system-tests/resources/register-participants.sh
@@ -5,5 +5,5 @@ PARTICIPANTS=(provider consumer-eu consumer-us)
 # Register dataspace participants
 for i in "${PARTICIPANTS[@]}"; do
     echo "Registering $i"
-    java -jar $REGISTRATION_SERVICE_CLI_JAR_PATH -s='http://localhost:8184/api' participants add --request='{ "name": "'$i'", "supportedProtocols": [ "ids-multipart" ], "url": "http://'$i':8282" }'
+    java -jar $REGISTRATION_SERVICE_CLI_JAR_PATH -d="did:web:$i-did-server:$i" -k=resources/vault/$i/private-key.pem -s='http://localhost:8184/api' participants add --ids-url "http://${{ env.EDC_HOST }}:8282"
 done
