02a5ab10a2cfa4719ff8d17d063ec9f3ca02be21
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-07-22 16:56:51 +0200

    use docker for cli tools
    Use docker for cli tools
    
    add execute permission explicitly

--- system-tests/resources/register-participants.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PARTICIPANTS=(provider consumer-eu consumer-us)
-
-# Register dataspace participants
-for i in "${PARTICIPANTS[@]}"; do
-    echo "Registering $i"
-    java -jar $REGISTRATION_SERVICE_CLI_JAR_PATH -d="did:web:did-server:$i" -k=system-tests/resources/vault/$i/private-key.pem -s='http://localhost:8184/authority' participants add --ids-url "http://$i:8282"
-done
