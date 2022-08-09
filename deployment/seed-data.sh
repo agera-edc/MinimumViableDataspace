2121e87676d3af494800c745c6f2d266fa422ab4
Peeyush Chandel <555114+cpeeyush@users.noreply.github.com>
2022-08-09 12:53:53 +0200

    Update to use same delimiter in shell script as used in other shell script  - prf feedback

--- deployment/seed-data.sh
+++ deployment/seed-data.sh
@@ -2,28 +2,25 @@
 
 set -euxo pipefail
 
-function split_by_comma() {
-  local -r val="$1"
-  echo "${val}" | tr "," "\n"
-}
+# Split env vars into an array using delimiter ':'
 
-arr_participant_ids=(`split_by_comma $PARTICIPANT_ID`)
-arr_assets_storage_accounts=(`split_by_comma $ASSETS_STORAGE_ACCOUNT`)
-arr_edc_hosts=(`split_by_comma $EDC_HOST`)
+ParticipantIdArray=(${PARTICIPANT_ID//:/ })
+AssetsStorageAccountArray=(${ASSETS_STORAGE_ACCOUNT//:/ })
+EdcHostArray=(${EDC_HOST//:/ })
 
-if [ ${#arr_participant_ids[@]} -ne ${#arr_assets_storage_accounts[@]} ] || [ ${#arr_assets_storage_accounts[@]} -ne ${#arr_edc_hosts[@]} ]; then
-    echo "PARTICIPANT_ID,ASSETS_STORAGE_ACCOUNT and EDC_HOST must be of equal length"
-    exit 1
+if [ ${#ParticipantIdArray[@]} -ne ${#AssetsStorageAccountArray[@]} ] || [ ${#AssetsStorageAccountArray[@]} -ne ${#EdcHostArray[@]} ]; then
+  echo "PARTICIPANT_ID,ASSETS_STORAGE_ACCOUNT and EDC_HOST must be of equal length"
+  exit 1
 fi
 
-for i in "${!arr_edc_hosts[@]}"; do
-    echo "Seeding data for Participant ID: ${arr_participant_ids[$i]}, Assets Storage Account: ${arr_assets_storage_accounts[$i]}, EDC Host: ${arr_edc_hosts[$i]}"
+for i in "${!EdcHostArray[@]}"; do
+  echo "Seeding data for Participant ID: ${ParticipantIdArray[$i]}, Assets Storage Account: ${AssetsStorageAccountArray[$i]}, EDC Host: ${EdcHostArray[$i]}"
 
-    newman run \
-      --folder "Publish Master Data" \
-      --env-var data_management_url="http://${arr_edc_hosts[$i]}:9191/api/v1/data" \
-      --env-var storage_account="${arr_assets_storage_accounts[$i]}" \
-      --env-var participant_id="${arr_participant_ids[$i]}" \
-      --env-var api_key="$API_KEY" \
-      deployment/data/MVD.postman_collection.json
+  newman run \
+    --folder "Publish Master Data" \
+    --env-var data_management_url="http://${EdcHostArray[$i]}:9191/api/v1/data" \
+    --env-var storage_account="${AssetsStorageAccountArray[$i]}" \
+    --env-var participant_id="${ParticipantIdArray[$i]}" \
+    --env-var api_key="$API_KEY" \
+    deployment/data/MVD.postman_collection.json
 done
