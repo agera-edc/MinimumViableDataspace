6813c9477f0964ce721e66e9e3c9081508289b49
Marc Gomez <marcgomez@microsoft.com>
2022-08-16 16:08:31 +0200

    Participant registration after VC seeding

--- system-tests/resources/cli-tools/entrypoint.sh
+++ system-tests/resources/cli-tools/entrypoint.sh
@@ -13,6 +13,19 @@ for participant in "${PARTICIPANTS[@]}"; do
     region=${participantArray[1]}
     participantDid="did:web:did-server:$participantName"
 
+    for subject in '"region": "'$region'"' '"gaiaXMember": "true"'
+    do
+         echo "Seeding VC for $participantName: $subject"
+         vcId=$(uuidgen)
+         java -jar identity-hub-cli.jar \
+                -s="http://$participantName:8181/api/identity-hub" \
+                 vc add \
+                -c='{"id":"'$vcId'","credentialSubject":{'"$subject"}'}' \
+                -b="$participantDid" \
+                -i="did:web:did-server:gaia-x" \
+                -k="/resources/vault/gaia-x/private-key.pem"
+    done
+
     echo "Registering $participantName"
     java -jar registration-service-cli.jar \
                 -d="did:web:did-server:registration-service" \
@@ -21,17 +34,4 @@ for participant in "${PARTICIPANTS[@]}"; do
                 -c="$participantDid" \
                  participants add \
                 --ids-url "http://$participantName:8282"
-
-   for subject in '"region": "'$region'"' '"gaiaXMember": "true"'
-   do
-     echo "Seeding VC for $participantName: $subject"
-     vcId=$(uuidgen)
-     java -jar identity-hub-cli.jar \
-                 -s="http://$participantName:8181/api/identity-hub" \
-                 vc add \
-                 -c='{"id":"'$vcId'","credentialSubject":{'"$subject"}'}' \
-                -b="$participantDid" \
-                 -i="did:web:did-server:gaia-x" \
-                 -k="/resources/vault/gaia-x/private-key.pem"
-   done
 done
