3941fa031463d5fb17692162fbd536d755614f3a
Peter Kirchner <peter.kirchner@microsoft.com>
2022-08-18 14:09:52 +0200

    refactor: remove temporary wait step
    + remove wait for onboarding from entrypoint.sh

--- system-tests/resources/cli-tools/entrypoint.sh
+++ system-tests/resources/cli-tools/entrypoint.sh
@@ -3,38 +3,6 @@
 # stop on error
 set -euo pipefail
 
-function waitForOnboarding() {
-  local participantName="$1"
-  local participantDid="$2"
-  echo "Fetching $participantName onboarding status..."
-  retryCount=0
-  maxRetryCount=30
-  onboardingCompleted=false
-  while [ $retryCount -lt $maxRetryCount ]; do
-      cmd="java -jar registration-service-cli.jar \
-                      -d=did:web:did-server:registration-service \
-                      --http-scheme \
-                      -k=/resources/vault/$participantName/private-key.pem \
-                      -c=$participantDid \
-                      participants get"
-      status=$($cmd|jq ".status")
-      echo "Status is $status"
-      if [ "$status" == "\"ONBOARDED\"" ]; then
-          echo "$participantName is onboarded successfully"
-          onboardingCompleted=true
-          break
-      else
-          echo "Onboarding is not completed yet for $participantName. Waiting for 1 seconds..."
-          sleep 1
-      fi
-      retryCount=$((retryCount+1))
-  done
-  if [ "$onboardingCompleted" == false ]; then
-      echo "$participantDid onboarding is not completed yet. Exiting..."
-      exit 1
-  fi
-}
-
 function registerParticipant() {
   local participantName="$1"
   local participantDid="$2"
@@ -46,9 +14,6 @@ function registerParticipant() {
               -k=/resources/vault/$participantName/private-key.pem \
               -c="$participantDid" \
                participants add
-
-  # wait for the participant to be onboarded
-  waitForOnboarding "$participantName" "$participantDid"
 }
 
 function seedVerifiedCredentials() {
